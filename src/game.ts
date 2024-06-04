8db32d3c9fd42b51fb8835e86553a076e7c06c39
Nguyen Dung Minh <97732725+MinhDungNguyen2003@users.noreply.github.com>
2024-06-04 17:46:15 +0700

    finish

--- src/game.ts
+++ src/game.ts
@@ -4,9 +4,15 @@ function loop(lastTime: number): void {
     const curTime = Date.now()
 
     gameManager.render()
-    gameManager.update(curTime - lastTime)
+    let frameTimeDelta = curTime - lastTime
+
     lastTime = Date.now()
-    
+    while (frameTimeDelta >= 1000 / 60) {
+        gameManager.update(1000 / 60)
+        frameTimeDelta -= 1000 / 60
+    }
+    gameManager.update(frameTimeDelta)
+
     requestAnimationFrame(() => loop(lastTime))
 }
 
